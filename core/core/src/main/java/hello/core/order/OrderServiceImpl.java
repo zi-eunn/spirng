4beee813a9966ea2464ccb8d614540eb18d26fd8
zi-eunn <jieunkim1117@gmail.com>
2025-05-12 23:47:57 +0900

    주문과 할인 도메인 개발

---
+++ core/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,25 @@
+package hello.core.order;
+
+import hello.core.Member.Member;
+import hello.core.Member.MemberRepository;
+import hello.core.Member.MemoryMemberRepository;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+
+public class OrderServiceImpl implements OrderService{
+
+    //memberRepository에서 회원 찾아야 함
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //할인된 가격 계산 해야하니까 -> 고정 할인 정책
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        //멤버 찾고
+        Member member = memberRepository.findById(memberId);
+        //할인은 discountPolicy니가 던져줘라 난 모르겠다. -> 단일 체계 원칙 잘 설계됨
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
