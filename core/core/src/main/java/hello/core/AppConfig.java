2d900bfbfda922143be47b9c91ad11d7fff22446
zi-eunn <jieunkim1117@gmail.com>
2025-05-15 14:45:54 +0900

    appConfig refactoring

--- core/core/src/main/java/hello/core/AppConfig.java
+++ core/core/src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.Member.MemberRepository;
 import hello.core.Member.MemberService;
 import hello.core.Member.MemberServiceImpl;
 import hello.core.Member.MemoryMemberRepository;
@@ -9,13 +10,31 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+//    public MemberService memberService() {
+//        return new MemberServiceImpl(new MemoryMemberRepository());
+//    }
+//    public OrderService orderService() {
+//        return new OrderServiceImpl(
+//                new MemoryMemberRepository(),
+//                new FixDiscountPolicy()
+//        );
+//    }
+
+    //리팩토링 ctrl + alt + M
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
+
     public OrderService orderService() {
-        return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy()
-        );
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    //new memberRepository() 중복을 막기 위해 리팩토링
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public FixDiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
