b9dbcd215f25322f25d15911c89de7e4cfd5087c
zi-eunn <jieunkim1117@gmail.com>
2025-05-22 10:43:04 +0900

    싱글톤 패턴

--- core/core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -2,6 +2,7 @@ package hello.core.singleton;
 
 import hello.core.AppConfig;
 import hello.core.Member.MemberService;
+import net.minidev.json.JSONUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -25,5 +26,26 @@ public class SingletonTest {
         //memberService1 != memberService2
         //콘솔 출력으로 확인했지만 항상 자동화 해야함!!
         assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    public void singletonServiceTest() {
+        //private로 생성자를 막아두었다. 컴파일 오류 발생
+        //new SingletonService();
+
+        //1. 조회: 호출할 때마다 같은 객체 반환
+        SingletonService singletonService1 = SingletonService.getInstance();
+        //2. 조회: "
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        //참조값이 같은 것을 확인
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        // singletonService1 == singletonService2
+        assertThat(singletonService1).isSameAs(singletonService1);
+
+        singletonService1.login();
     }
 }
